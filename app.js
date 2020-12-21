0f6d444d5c488f393b6556fb511a5f96a7c1f8d5
zu <luigi@MacBook-Pro-de-zu.local>
2020-12-21 18:30:10 -0300

    Angular - Compilacion y configuracion TS

---
+++ app.js
@@ -0,0 +1,9 @@
+(function () {
+    function saludar(nombre) {
+        console.table('Hola ' + nombre);
+    }
+    var wolverine = {
+        nombre: 'Logan'
+    };
+    saludar(wolverine.nombre);
+})();
---
+++ app.ts
@@ -0,0 +1,12 @@
+//Funcion anonima autoinvocada
+(function(){
+    function saludar(nombre: string) {
+        console.table('Hola ' + nombre);
+    }
+    
+    const wolverine = {
+        nombre: 'Logan'
+    };
+    
+    saludar( wolverine.nombre);
+})()
