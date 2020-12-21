98f8c59959c7f92a9206ba52677385fe8609ba71
zu <luigi@MacBook-Pro-de-zu.local>
2020-12-21 18:51:23 -0300

    Angular - Limpieza master

--- app.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    function saludar(nombre) {
-        console.table('Hola ' + nombre);
-    }
-    var wolverine = {
-        nombre: 'Logan'
-    };
-    saludar(wolverine.nombre);
-})();
