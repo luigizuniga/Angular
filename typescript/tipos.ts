270710b2278bb9cf0dee373f9bd94ce2ff61b4ed
zu <luigi@MacBook-Pro-de-zu.local>
2020-12-22 18:44:27 -0300

    Angular - Tipos de datos y configuracion exclude directorios

---
+++ typescript/tipos.ts
@@ -0,0 +1,22 @@
+(function(){
+    let mensaje: string =  'Hello';
+    let numero: number = 30;
+    let booleano: boolean = true;
+    let hoy: Date = new Date();
+
+    let anything;
+    anything = mensaje;
+    anything = numero;
+    anything = booleano;
+    anything = hoy;
+
+    let spiderman = {
+        nombre: 'Peter',
+        edad: 24
+    }
+
+    spiderman = {
+        nombre: 'Alex',
+        edad: 31
+    }
+})();
