287ddb173cf00f92e8b9e2240115324b886a029e
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-03 13:40:21 -0300

    Angular - Parametros y literal template

--- typescript/tipos.ts
+++ typescript/tipos.ts
@@ -1,8 +1,10 @@
 (function(){
+    //Declaracion y asignacion de variables 
     let mensaje: string =  'Hello';
     let numero: number = 30;
     let booleano: boolean = true;
     let hoy: Date = new Date();
+
 
     let anything;
     anything = mensaje;
@@ -10,13 +12,33 @@
     anything = booleano;
     anything = hoy;
 
+    
+    //Declaracion de Objeto
     let spiderman = {
-        nombre: 'Peter',
-        edad: 24
+        nombre: '',
+        edad: 0
     }
 
     spiderman = {
         nombre: 'Alex',
         edad: 31
     }
+    
+    //Template Literales 
+    const salida = `${ mensaje } edad ${ spiderman.edad } ${ spiderman.nombre } año ${ hoy.getUTCFullYear() }`;
+    console.table( salida );
+
+    //Parametros opcionales, Obligatorios y por defecto
+    function activar(quien: string, 
+                     objeto: string = 'batiseñal', 
+                     momento?: string) {
+        if( momento ){
+            console.log(`${ quien } activó la ${ objeto } en la ${ momento }`);
+        }else{
+            console.log(`${ quien } activó la ${ objeto }`);
+        }
+    }
+
+    activar('Gordon');
+    activar('Gordon','batiseñal','noche');
 })();
