a02d08d4b92d1dd2e1cb7b99ef08da0703219d14
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-10 13:47:04 -0300

    Angular - Decoradores de clase

--- src/clases/xmen.class.ts
+++ src/clases/xmen.class.ts
@@ -1,3 +1,12 @@
+
+// Los Decoradores no son de uso recurrente,
+// sirven para añadir funcionalidad a clase, añadiendo caracteristicas y funcionalidades propias
+
+function imprimirConsola( contructorClase: Function ) {
+     console.log( contructorClase );   
+}
+
+@imprimirConsola
 export class Xmen  {
 
     constructor(
